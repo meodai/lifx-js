6203ad33c85c8fc26e479da35b44ea205c804dea
David Aerne <meodai@gmail.com>
2015-10-17 20:28:35 +0200

    adds color-thief and getusermedia-js to client js

--- client-js/main.js
+++ client-js/main.js
@@ -1,7 +1,11 @@
 var chroma = require('chroma-js');
 var $ = require('jquery');
+var userMedia = require('getusermedia-js');
+var ct = require('color-thief');
 var socket = io();
 
 $(document).on('change', '#color', function(){
     socket.emit('color change', $(this).val());
 });
+
+
